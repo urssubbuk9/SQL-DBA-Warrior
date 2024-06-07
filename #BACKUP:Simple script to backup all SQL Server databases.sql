3c16e6c8ed0c950e98e88ce4ee788520a077c2af
Subbu CN <45705730+urssubbuk9@users.noreply.github.com>
2024-06-07 22:38:18 +0530

    Update #BACKUP:Simple script to backup all SQL Server databases.sql

--- #BACKUP:Simple script to backup all SQL Server databases.sql
+++ #BACKUP:Simple script to backup all SQL Server databases.sql
@@ -30,3 +30,37 @@ END
  
 CLOSE db_cursor   
 DEALLOCATE db_cursor
+
+============================ SCRIPT 2 ========================
+DECLARE @DatabaseName sysname
+DECLARE @BackupPath nvarchar(500)
+DECLARE @SQL nvarchar(max)
+
+-- Set the backup path
+SET @BackupPath = 'C:\SQL_DATA\Backup\' -- Replace with your desired backup path
+
+-- Create a cursor to loop through all user databases
+DECLARE db_cursor CURSOR FOR
+SELECT name
+FROM sys.databases
+WHERE database_id > 4 -- Exclude system databases
+
+OPEN db_cursor
+
+FETCH NEXT FROM db_cursor INTO @DatabaseName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    -- Generate the dynamic SQL statement for each database
+    DECLARE @BackupFileName nvarchar(500)
+    SET @BackupFileName = @BackupPath + @DatabaseName + '_' + REPLACE(CONVERT(nvarchar(16), GETDATE(), 120), ':', '_') + '.bak'
+    SET @SQL = 'BACKUP DATABASE ' + QUOTENAME(@DatabaseName) + ' TO DISK = ''' + @BackupFileName + ''' WITH COMPRESSION'
+
+    -- Execute the backup command
+    EXEC sp_executesql @SQL
+
+    FETCH NEXT FROM db_cursor INTO @DatabaseName
+END
+
+CLOSE db_cursor
+DEALLOCATE db_cursor
